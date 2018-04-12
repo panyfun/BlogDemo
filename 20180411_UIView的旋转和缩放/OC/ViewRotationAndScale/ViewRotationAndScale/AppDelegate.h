a221c9fcd6c518485dd4778041316a7ccd5cdb6a
pany <panyz666@gmail.com>
2018-04-12 17:54:38 +0800

    新开一篇——uiview的旋转和缩放

---
+++ 20180411_UIView的旋转和缩放/OC/ViewRotationAndScale/ViewRotationAndScale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewRotationAndScale
+//
+//  Created by muma on 2018/4/11.
+//  Copyright © 2018年 pany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20180411_UIView的旋转和缩放/OC/ViewRotationAndScale/ViewRotationAndScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewRotationAndScale
+//
+//  Created by muma on 2018/4/11.
+//  Copyright © 2018年 pany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
