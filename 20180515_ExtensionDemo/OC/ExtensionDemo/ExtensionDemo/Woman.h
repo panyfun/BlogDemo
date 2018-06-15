98960736ca14369926d527d65de1f063c8864f87
pany <panyz666@gmail.com>
2018-06-15 17:09:33 +0800

    添加ExtensionDemo

---
+++ 20180515_ExtensionDemo/OC/ExtensionDemo/ExtensionDemo/Woman.h
@@ -0,0 +1,15 @@
+//
+//  Woman.h
+//  ExtensionDemo
+//
+//  Created by Pany on 2018/6/15.
+//  Copyright © 2018年 Pany. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Woman : NSObject
+
+@property (nonatomic, readonly) NSString *name;
+
+@end
---
+++ 20180515_ExtensionDemo/OC/ExtensionDemo/ExtensionDemo/Woman.m
@@ -0,0 +1,15 @@
+//
+//  Woman.m
+//  ExtensionDemo
+//
+//  Created by Pany on 2018/6/15.
+//  Copyright © 2018年 Pany. All rights reserved.
+//
+
+#import "Woman.h"
+
+//#import "Woman+Secret.h"
+
+@implementation Woman
+
+@end
